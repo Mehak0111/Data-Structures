cc5d079a81a4697dc203fc38a63a076f5ca0724d
unknown <baggamehak24@gmail.com>
2022-12-20 12:31:09 +0530

    stackBasics

--- Stack/stackBasic.cpp
+++ Stack/stackBasic.cpp
@@ -1,20 +1,82 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-int main(){
-	//creation of stack
-	stack<int> s;
-	//push operation
-	s.push(2);
-	s.push(3);
-	//pop
-	s.pop();
+class Stack{
+	public:
+		int *arr;
+		int top;
+		int size;
+		
+    //behaviour
+    Stack(int size){
+    	this->size=size;
+    	arr=new int[size];
+    	top=-1;
+	}
 	
-	cout<<"Printing top element "<<s.top()<<endl;
-	if(s.empty()){
-		cout<<"Stack is empty"<<endl;
+	void push(int element){
+		if(size-top>1){
+			top++;
+			arr[top]=element;
+		}
+		else{
+			cout<<"Stack Overflow"<<endl;
+		}
 	}
-	else{
-		cout<<"Stack is not empty"<<endl;
+	
+	void pop(){
+		if(top>=0){
+			top--;
+		}
+		else{
+			cout<<"Stack Underflow"<<endl;
+		}
 	}
-}
+	
+	int peek(){
+		if(top>=0)
+		return arr[top];
+		else{
+			cout<<"Stack is empty"<<endl;
+			return -1;
+		}
+	}
+	
+	bool isEmpty(){
+		if(top==-1){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+};
+int main(){
+//	//creation of stack
+//	stack<int> s;
+//	//push operation
+//	s.push(2);
+//	s.push(3);
+//	//pop
+//	s.pop();
+//	
+//	cout<<"Printing top element "<<s.top()<<endl;
+//	if(s.empty()){
+//		cout<<"Stack is empty"<<endl;
+//	}
+//	else{
+//		cout<<"Stack is not empty"<<endl;
+//	}
+
+   Stack st(5);
+   st.push(22);
+   st.push(43);
+   st.push(44);
+   cout<<st.peek()<<endl;
+   st.pop();
+   cout<<st.peek()<<endl;
+   st.pop();
+   cout<<st.peek()<<endl;
+   st.pop();
+   cout<<st.peek()<<endl;
+   }
