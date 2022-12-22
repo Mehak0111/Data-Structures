e21ca7593fe601ed56304a6061a689babb06f441
unknown <baggamehak24@gmail.com>
2022-12-22 09:28:59 +0530

    taking linked list as user-input

--- Linked List/BasicLL.cpp
+++ Linked List/BasicLL.cpp
@@ -10,19 +10,46 @@ class Node{
 			this->next=NULL;
 		}
 };
+Node *takeinput(){
+	int data;
+	cin>>data;
+	Node* head=NULL;
+	Node* tail=NULL;
+	while(data!=-1){
+		Node* newnode=new Node(data);
+		if(head==NULL){
+			head=newnode;
+			tail=newnode;
+		}
+		else{
+			tail->next=newnode;
+			tail=tail->next;
+		}
+		cin>>data;
+	}
+	return head;
+}
+void print(Node* head){
+	while(head!=NULL){
+		cout<<head->data<<" ";
+		head=head->next;
+	}
+}
 int main()
 {
-	//statically
-Node n1(1);
-Node *head=&n1;
-Node n2(2);
-n1.next=&n2;
-cout<<n1.data<<" "<<n2.data<<endl;
-
-
- //dynamically
-	Node* n3=new Node(10);
-    head=n3;
-    Node *n4=new Node(20);
-    n3->next=n4;
+//statically
+//Node n1(1);
+//Node *head=&n1;
+//Node n2(2);
+//n1.next=&n2;
+//cout<<n1.data<<" "<<n2.data<<endl;
+//
+//
+// dynamically
+//	Node* n3=new Node(10);
+//    head=n3;
+//    Node *n4=new Node(20);
+//    n3->next=n4;
+  Node* head=takeinput();
+  print(head);
 }
